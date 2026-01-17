88f30c14ef33f40a5eb80ff1eb03298f6a2fbab8
Hyeon Lee <iggpieei@gmail.com>
2026-01-17 15:10:29 +0900

    Solve 0x01

--- 0x01/test.cpp
+++ 0x01/test.cpp
@@ -2,19 +2,37 @@
 using namespace std;
 
 int func1(int N){
-  return -1;
+  int sum = 0;
+  for (int i = 1; i <= N; i++){
+    if (i % 3 == 0 || i % 5 == 0){
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 int func2(int arr[], int N){
-  return -1;
+  for (int i= 0; i < N; i++){
+    for (int j = 1; j < N; j++){
+      if (arr[i] + arr[j] == 100) return 1;
+    }
+  }
+  return 0;
 }
 
 int func3(int N){
-  return -1;
+  for (int i = 0; i * i <= N; i++){
+    if (i * i == N) return 1;
+  }
+  return 0;
 }
 
 int func4(int N){
-  return -1;
+  int mul = 1;
+  while (2 * mul <= N){
+    mul *= 2;
+  }
+  return mul;
 }
 
 void test1(){
