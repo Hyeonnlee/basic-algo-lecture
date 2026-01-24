32d7f2fdb72dbf79c2c7673a73a397d8a1a9b1ef
Hyeonnlee <iggpieei@gmail.com>
2026-01-24 15:12:43 +0900

    Solve 2480

---
+++ 0x02/solutions/2480.py
@@ -0,0 +1,8 @@
+num = [x for x in map(int, input().split())]
+num.sort()
+if num[0] == num[1] and num[1] == num[2]:
+    print(10000 + num[0] * 1000)
+elif num[0] == num[1] or num[1]  == num[2]:
+    print(1000 + num[1] * 100)
+else:
+    print(num[2] * 100)
